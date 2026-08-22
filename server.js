08d3e888dd8b6a5f198d47da067b48fabc1fef62
sofialarraya326-source <sofialarraya326@gmail.com>
2026-08-22 14:28:50 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 /* =========================
    CONFIGURACIÓN DEL JUEGO
